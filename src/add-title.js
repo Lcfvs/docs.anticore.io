ca9f35235d1e383003656a387ac327bc2a635b5d
Lcf.vs <lcf.vs@hotmail.com>
2019-02-08 20:29:07 +0100

    Cleaning add-title.js

--- src/add-title.js
+++ src/add-title.js
@@ -1,9 +1,6 @@
 import { anticore } from 'anticore'
 
 anticore.on('body', (body, next) => {
-  body.innerHTML =
-    '<h1>Hello Luce :-)</h1>' +
-    "Faudrait voir si ça marche aussi quand j'suis pas connecté" +
-    "<p style='color:red'>nb:kb</p>"
+  body.innerHTML = '<h1>This is the title</h1>'
   next()
 })
