24bb48d8b9e73cd8fc5c2bcf1c96ecbbb960ad16
Lcfvs <michael.rouges@gmail.com>
2019-02-19 04:47:39 +0100

    Rewrites the layout and landing page style refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // Import your own middlewares here
 
 // Let the following lines at the end of this file
-import "./styles";
 import "anticore/middleware/main/mono";
 import { anticore } from "anticore";
 import reloader from "./reloader";
